6959a55290a75887c71408ffdce0d32ea41e3641
hushunding <hushunding@aliyun.com>
2018-09-19 16:28:11 +0800

    移除本分支无关

--- src/app/param-table-component.ts
+++
@@ -1,7 +0,0 @@
-import { NormalParamSchemtic, ParamValue, ParamDataSet } from './Param/ParamTmpl';
-
-export interface ParamTableComponent {
-    ParamSchemtic: NormalParamSchemtic;
-    ParamDataSet: ParamDataSet<ParamValue>;
-    Editing: boolean;
-}
